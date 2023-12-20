b12d766d9f47318ae5917193769ce624f108345e
blockchain5992 <ayushporwal91@gmail.com>
2023-12-20 17:51:17 +0530

    feat: referral registry contract

--- contracts/Registry/ReferralRegistry.sol
+++ contracts/Registry/ReferralRegistry.sol
@@ -1,59 +1,187 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.20;
 
-contract ReferralRegistry {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title ReferralRegistry
+ * @dev A Solidity smart contract for managing a referral registry system with affiliates, brokers, traders, and exchanges.
+ */
+contract ReferralRegistry is Ownable {
+    /**
+     * @dev Struct representing a Trader with registration status and an affiliated code.
+     */
     struct Trader {
-        // bool value for checking trader is registered or not
-        bool isRegisterd;
-        // affiliatedCode associated with trader
-        bytes32 affiliatedCode;
+        bool isRegistered; // Flag indicating whether the trader is registered or not.
+        bytes32 affiliatedCode; // Affiliated code associated with the trader.
     }
 
+    /**
+     * @dev Struct representing a Broker with whitelist status and a mapping of unique codes to Dex addresses.
+     */
     struct Broker {
-        // bool value for checking broker is whitelisted or not
-        bool isWhiteListed;
-        // mapping of unique code to Dexs address
-        mapping(bytes32 => address) Dexs;
+        bool isWhiteListed; // Flag indicating whether the broker is whitelisted or not.
+        mapping(bytes32 => address) Dexs; // Mapping of unique codes to Dex addresses.
     }
 
+    /**
+     * @dev Struct representing an Affiliate with registration status and a unique affiliate code.
+     */
     struct Affiliate {
-        // bool value for checking affiliate is registered or not
-        bool isRegistered;
-        // unique affiliatedCode code
-        bytes32 affiliateCode;
+        bool isRegistered; // Flag indicating whether the affiliate is registered or not.
+        bytes32 affiliateCode; // Unique affiliated code associated with the affiliate.
     }
 
+    /**
+     * @dev Struct representing an Exchange with whitelist status and a unique exchange code.
+     */
     struct Exchange {
-        // bool value for checking Exchange is whitelisted or not
-        bool isWhiteListed;
-        // unique exchange code
-        bytes32 exchangeCode;
+        bool isWhiteListed; // Flag indicating whether the exchange is whitelisted or not.
+        bytes32 exchangeCode; // Unique exchange code associated with the exchange.
     }
 
-    // storing trader address to unique affiliate code
+    /**
+     * @dev Mapping of trader addresses to Trader information.
+     */
     mapping(address => Trader) public TraderRegister;
-    // storing broker address to Broker info
+
+    /**
+     * @dev Mapping of broker addresses to Broker information.
+     */
     mapping(address => Broker) public BrokerRegister;
-    // storing affiliate address to Affiliate info
+
+    /**
+     * @dev Mapping of affiliate addresses to Affiliate information.
+     */
     mapping(address => Affiliate) public AffiliateRegister;
-    // storing exchange address to Exchange info
-    mapping(string => Exchange) public ExchangeRegister;
-    // storing already registered code to prevent from duplicate code
+
+    /**
+     * @dev Mapping of exchange addresses to Exchange information.
+     */
+    mapping(address => Exchange) public ExchangeRegister;
+
+    /**
+     * @dev Mapping of already registered codes to prevent duplicate codes.
+     */
     mapping(bytes32 => bool) public RegisteredReferralCode;
 
-    constructor() {}
+    /**
+     * @dev Event emitted when a unique affiliate code is created.
+     */
+    event AffiliateCodeCreated(
+        address indexed creator,
+        bytes32 code,
+        uint256 timestamp
+    );
 
-    // function for creating unique referral code
-    // broker can create multiple codes unique to each exchanges
-    function createReferralCode(bytes32 _code, bool _isBroker) external {}
+    /**
+     * @dev Event emitted when a broker code is created for a specific exchange.
+     */
+    event BrokerCodeCreated(
+        address indexed broker,
+        address exchange,
+        bytes32 code,
+        uint256 timestamp
+    );
 
-    // function for setting affiliate code for getting fee discount
-    // used only for first time and it forever stores in the contract
-    function setTraderCode(bytes32 _code) external {}
+    /**
+     * @dev Event emitted when a trader code is registered.
+     */
+    event TraderCodeRegistered(
+        address indexed trader,
+        bytes32 code,
+        uint256 timestamp
+    );
 
-    // function for whitelist broker
-    function registerBroker(address _broker) external {}
+    /**
+     * @dev Constructor initializing the contract and setting the owner.
+     */
+    constructor() Ownable(msg.sender) {}
 
-    // function for whitelist exchange those are not coming from broker
-    function registerExchange(address _exchange, bytes32 _code) external {}
+    /**
+     * @dev Function for creating a unique affiliate code.
+     * @param code The unique code to be associated with the affiliate.
+     */
+    function createAffiliateCode(bytes32 code) external {
+        require(
+            !RegisteredReferralCode[code],
+            "ReferralRegistry: Code already registered"
+        );
+        RegisteredReferralCode[code] = true;
+        require(
+            !AffiliateRegister[msg.sender].isRegistered,
+            "ReferralRegistry: Affiliate already registered"
+        );
+        AffiliateRegister[msg.sender].isRegistered = true;
+        AffiliateRegister[msg.sender].affiliateCode = code;
+        emit AffiliateCodeCreated(msg.sender, code, block.timestamp);
+    }
+
+    /**
+     * @dev Function for creating a unique broker code for a specific exchange.
+     * @param exchange The address of the exchange.
+     * @param code The unique code to be associated with the broker and exchange.
+     */
+    function createBrokerCode(address exchange, bytes32 code) external {
+        require(BrokerRegister[msg.sender].isWhiteListed,
+            "ReferralRegistry: Broker is not whitelisted"
+        );
+        require(
+            !RegisteredReferralCode[code],
+            "ReferralRegistry: Code already registered"
+        );
+        RegisteredReferralCode[code] = true;
+        BrokerRegister[msg.sender].isWhiteListed = true;
+        BrokerRegister[msg.sender].Dexs[code] = exchange;
+        emit BrokerCodeCreated(msg.sender, exchange, code, block.timestamp);
+    }
+
+    /**
+     * @dev Function for setting a trader code for fee discounts.
+     * @param code The unique code to be associated with the trader.
+     */
+    function setTraderCode(bytes32 code) external {
+        require(
+            !TraderRegister[msg.sender].isRegistered,
+            "ReferralRegistry: Affiliate already registered"
+        );
+        TraderRegister[msg.sender].isRegistered = true;
+        TraderRegister[msg.sender].affiliatedCode = code;
+
+        emit TraderCodeRegistered(msg.sender, code, block.timestamp);
+    }
+
+    /**
+     * @dev Function for managing broker participation (whitelisting).
+     * @param broker The address of the broker.
+     * @param isWhiteListed A flag indicating whether the broker should be whitelisted or not.
+     */
+    function manageBroker(
+        address broker,
+        bool isWhiteListed
+    ) external onlyOwner {
+        require(broker != address(0), "ReferralRegistry: Can't be zero Add");
+        BrokerRegister[broker].isWhiteListed = isWhiteListed;
+    }
+
+    // TODO: exchange add can't be EOA
+    /**
+     * @dev Function for managing exchanges not coming from brokers.
+     * @param exchange The address of the exchange.
+     * @param code The unique code to be associated with the exchange.
+     * @param isWhiteListed A flag indicating whether the exchange should be whitelisted or not.
+     */
+    function manageExchange(
+        address exchange,
+        bytes32 code,
+        bool isWhiteListed
+    ) external onlyOwner {
+        require(exchange != address(0), "ReferralRegistry: Can't be zero Add");
+        require(
+            !ExchangeRegister[exchange].isWhiteListed,
+            "ReferralRegistry: Exchange already registered"
+        );
+        ExchangeRegister[exchange].isWhiteListed = isWhiteListed;
+        ExchangeRegister[exchange].exchangeCode = code;
+    }
 }
